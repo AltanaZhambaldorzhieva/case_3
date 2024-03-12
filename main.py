be12591e8c0308956e8b7dc48ea1b942ce172578
Darialr <darial.ryaguzova@gmail.com>
2024-03-12 19:51:15 +0700

    def annual income

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import ru_local as ru
 MAX_MONTH = 12
 
 grade = int(input(f'{ru.CATEGORY}\n1.{ru.ALONE}\n2.{ru.FAMILY}\n3.{ru.SINGLE}\n{ru.GRADE}'))
+print(ru.ANNUAL_INCOME)
+
+def annual_income():
+    '''
+        The function determines the annual tax-free amount.
+    '''
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.INCOME} {ru.NAME[month]} [USD]: '))
+        amount += value
+    return amount
 
 
 def free_tax():
@@ -16,5 +27,4 @@ def free_tax():
         value = float(input(f'{ru.TEXT_FREE_TAX} {ru.NAME[month]} [USD]: '))
         amount += value
     return amount
-
 
