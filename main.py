0de7da23251e7e4a08736993cbec75e1d894ee4d
daryazaitseva <zaytsevaart@gmail.com>
2024-03-13 09:55:32 +0700

    changes

--- main.py
+++ main.py
@@ -5,91 +5,80 @@ import ru_local as ru
 
 MAX_MONTH = 12
 
-    
 
-def main():
+def annual_income():
     """
-        Main function.
-        :return: None
+        The function determines the amount of annual income.
     """
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.INCOME} {ru.NAME[month]} [USD]: '))
+        amount += value
+    return amount
 
-    def annual_income():
-        """
-            The function determines the amount of annual income.
-        """
-        amount = 0
-        for month in range(1, MAX_MONTH + 1):
-            value = float(input(f'{ru.INCOME} {ru.NAME[month]} [USD]: '))
-            amount += value
-        return amount
 
-    def free_tax():
-        """
-        The function determines the annual tax-free amount.
-        """
-        amount = 0
-        for month in range(1, MAX_MONTH + 1):
-            value = float(input(f'{ru.TEXT_FREE_TAX} {ru.NAME[month]} [USD]: '))
-            amount += value
-        return amount
+def free_tax():
+    """
+    The function determines the annual tax-free amount.
+    """
+    amount = 0
+    for month in range(1, MAX_MONTH + 1):
+        value = float(input(f'{ru.TEXT_FREE_TAX} {ru.NAME[month]} [USD]: '))
+        amount += value
+    return amount
 
-    revenue_tax = (amount1 - amount2)
+def subject(revenue_tax):
+     """
+        The function determines tax of subject.
+    """
+    if revenue_tax < 9076:
+        tax = 0.1 * revenue_tax
+        return tax
+    elif revenue_tax < 36901:
+        tax = 0.1 * 9075 + 0.15 * (revenue_tax - 9075)
+        return tax
+    elif revenue_tax < 89351:
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * (revenue_tax - 36900)
+        return tax
+    elif revenue_tax < 186351:
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * (revenue_tax - 89350)
+        return tax
+    elif revenue_tax < 405101:
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * (revenue_tax - 186350)
+        return tax
+    elif revenue_tax < 406_751:
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97_000 + 0.33 * 218750 + 0.35 * (
+                    revenue_tax - 405100)
+        return tax
+    else:
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * 218750 + 0.35 * 1650 + 0.396 * (revenue_tax - 406750)
+        return tax
 
-    def subject(revenue_tax):
-        """
-            The function determines tax of subject.
-        """
-        if revenue_tax < 9076:
-            tax = 0.1 * revenue_tax
-            return tax
-        elif revenue_tax < 36901:
-            tax = 0.1 * 9075 + 0.15 * (revenue_tax - 9075)
-            return tax
-        elif revenue_tax < 89351:
-            tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * (revenue_tax - 36900)
-            return tax
-        elif revenue_tax < 186351:
-            tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * (revenue_tax - 89350)
-            return tax
-        elif revenue_tax < 405101:
-            tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * (revenue_tax - 186350)
-            return tax
-        elif revenue_tax < 406_751:
-            tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97_000 + 0.33 * 218750 + 0.35 * (
-                        revenue_tax - 405100)
-            return tax
-        else:
-            tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * 218750 + 0.35 * 1650 + 0.396 * (
-                        revenue_tax - 406750)
-            return tax
-
-    def couple(revenue_tax):
-        """
-            The function determines tax of couple.
-        """
-        if revenue_tax < 18151:
-            tax = 0.1 * revenue_tax
-            return tax
-        elif revenue_tax < 73801:
-            tax = 0.1 * 18150 + 0.15 * (revenue_tax - 18150)
-            return tax
-        elif revenue_tax < 148851:
-            tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * (revenue_tax - 73800)
-            return tax
-        elif revenue_tax < 226851:
-            tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * (revenue_tax - 148850)
-            return tax
-        elif revenue_tax < 405101:
-            tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * (revenue_tax - 226850)
-            return tax
-        elif revenue_tax < 457601:
-            tax = 0.1 * 18_150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * (
-                        revenue_tax - 405100)
-            return tax
-        else:
-            tax = (0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * 52500
-                   + 0.396 * (revenue_tax - 457600))
-            return tax
+def couple(revenue_tax):
+    """
+        The function determines tax of couple.
+    """
+    if revenue_tax < 18151:
+        tax = 0.1 * revenue_tax
+        return tax
+    elif revenue_tax < 73801:
+        tax = 0.1 * 18150 + 0.15 * (revenue_tax - 18150)
+        return tax
+    elif revenue_tax < 148851:
+        tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * (revenue_tax - 73800)
+        return tax
+    elif revenue_tax < 226851:
+        tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * (revenue_tax - 148850)
+        return tax
+    elif revenue_tax < 405101:
+        tax = 0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * (revenue_tax - 226850)
+        return tax
+    elif revenue_tax < 457601:
+        tax = 0.1 * 18_150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * (revenue_tax - 405100)
+        return tax
+    else:
+        tax = (0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * 52500 + 0.396 * (revenue_tax - 457600))
+        return tax
 
     def parent(revenue_tax):
         """
@@ -120,14 +109,20 @@ def main():
             return tax
 
 
+def main():
+    """
+        Main function.
+        :return: None
+    """
+
     grade = int(input(f'{ru.CATEGORY}\n1.{ru.ALONE}\n2.{ru.FAMILY}\n3.{ru.SINGLE}\n{ru.GRADE}'))
-
     print(ru.ANNUAL_INCOME)
-    print(f'{ru.ANNUAL_INCOME_SUM} ${annual_income()}')
+    revenue = annual_income()
+    print(f'{ru.ANNUAL_INCOME_SUM} ${revenue}')
     print(ru.FREE_TAX)
-    print(f'{ru.FINAL_FREE_TAX} ${free_tax()}\n{ru.TAXABLE} ${revenue_tax}')
-
-
+    revenue_no_tax = free_tax()
+    revenue_tax = revenue - revenue_no_tax
+    print(f'{ru.FINAL_FREE_TAX} ${revenue_no_tax}\n{ru.TAXABLE} ${revenue_tax}')
 
 
 if __name__ == '__main__':
