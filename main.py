3a0beec553b8387406f1b626348ebbc7d1d4beca
daryazaitseva <zaytsevaart@gmail.com>
2024-03-13 10:41:13 +0700

    last

--- main.py
+++ main.py
@@ -27,8 +27,9 @@ def free_tax():
         amount += value
     return amount
 
+
 def subject(revenue_tax):
-     """
+    """
         The function determines tax of subject.
     """
     if revenue_tax < 9076:
@@ -51,8 +52,10 @@ def subject(revenue_tax):
                     revenue_tax - 405100)
         return tax
     else:
-        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * 218750 + 0.35 * 1650 + 0.396 * (revenue_tax - 406750)
+        tax = 0.1 * 9075 + 0.15 * 27825 + 0.25 * 52450 + 0.28 * 97000 + 0.33 * 218750 + 0.35 * 1650 + 0.396 * (
+                revenue_tax - 406750)
         return tax
+
 
 def couple(revenue_tax):
     """
@@ -77,36 +80,38 @@ def couple(revenue_tax):
         tax = 0.1 * 18_150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * (revenue_tax - 405100)
         return tax
     else:
-        tax = (0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * 52500 + 0.396 * (revenue_tax - 457600))
+        tax = (0.1 * 18150 + 0.15 * 55650 + 0.25 * 75050 + 0.28 * 78000 + 0.33 * 178250 + 0.35 * 52500 + 0.396 * (
+                revenue_tax - 457600))
         return tax
+
 
 def parent(revenue_tax):
     """
         The function determines tax of parent.
     """
-        if revenue_tax < 12951:
-            tax = 0.1 * revenue_tax
-            return tax
-        elif revenue_tax < 49401:
-            tax = 0.1 * 12950 + 0.15 * (revenue_tax - 12950)
-            return tax
-        elif revenue_tax < 127551:
-            tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * (revenue_tax - 49400)
-            return tax
-        elif revenue_tax < 206601:
-            tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * (revenue_tax - 127550)
-            return tax
-        elif revenue_tax < 405101:
-            tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * (revenue_tax - 206600)
-            return tax
-        elif revenue_tax < 432201:
-            tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * 198500 + 0.35 * (
-                        revenue_tax - 405100)
-            return tax
-        else:
-            tax = (0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * 198500 + 0.35 * 27100
-                   + 0.396 * (revenue_tax - 432200))
-            return tax
+    if revenue_tax < 12951:
+        tax = 0.1 * revenue_tax
+        return tax
+    elif revenue_tax < 49401:
+        tax = 0.1 * 12950 + 0.15 * (revenue_tax - 12950)
+        return tax
+    elif revenue_tax < 127551:
+        tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * (revenue_tax - 49400)
+        return tax
+    elif revenue_tax < 206601:
+        tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * (revenue_tax - 127550)
+        return tax
+    elif revenue_tax < 405101:
+        tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * (revenue_tax - 206600)
+        return tax
+    elif revenue_tax < 432201:
+        tax = 0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * 198500 + 0.35 * (
+                revenue_tax - 405100)
+        return tax
+    else:
+        tax = (0.1 * 12950 + 0.15 * 36450 + 0.25 * 78150 + 0.28 * 79050 + 0.33 * 198500 + 0.35 * 27100 + 0.396 * (
+                revenue_tax - 432200))
+        return tax
 
 
 def main():
@@ -124,17 +129,17 @@ def main():
     revenue_tax = revenue - revenue_no_tax
     print(f'{ru.FINAL_FREE_TAX} ${revenue_no_tax}\n{ru.TAXABLE} ${revenue_tax}')
     if grade == 1:
-        tax = subject()
-        print(f'{ru.TAX} ${tax}')
-        print(f'{ru.MOUNTHLY_TAX} ${tax / 12}')
+        tax = subject(revenue_tax)
+        print(f'{ru.TAX} ${round(tax, 2)}')
+        print(f'{ru.MOUNTHLY_TAX} ${round((tax / 12), 2)}')
     elif grade == 2:
-        tax = couple()
-        print(f'{ru.TAX} ${tax}')
-        print(f'{ru.MOUNTHLY_TAX} ${tax / 12}')
+        tax = couple(revenue_tax)
+        print(f'{ru.TAX} ${round(tax, 2)}')
+        print(f'{ru.MOUNTHLY_TAX} ${round((tax / 12), 2)}')
     else:
-        tax = parent()
-        print(f'{ru.TAX} ${tax}')
-        print(f'{ru.MOUNTHLY_TAX} ${tax / 12}')
+        tax = parent(revenue_tax)
+        print(f'{ru.TAX} ${round(tax, 2)}')
+        print(f'{ru.MOUNTHLY_TAX} ${round((tax / 12), 2)}')
 
 
 if __name__ == '__main__':
